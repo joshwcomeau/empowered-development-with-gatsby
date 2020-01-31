7308fcd497b5ee123f1b984db7b98571a595dbaf
Josh Comeau <joshwcomeau@gmail.com>
2020-01-31 16:20:44 -0500

    CatTweets test

---
+++ CatTweets.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import TweetEmbed from 'react-tweet-embed';
+
+
+const CatTweets = () => {
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('/.netlify/functions/cat-tweets')
+      .then(res => res.text())
+      .then(rawData => {
+        setData(rawData.split(';'))
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      })
+  }, [])
+
+  return (
+    <Wrapper>
+      {loading && "Loading..."}
+      {data && data.map(tweetId => {
+        return (
+          <TweetEmbed
+            key={tweetId}
+            id={tweetId}
+          />
+        )
+      })}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+
+`;
+
+export default CatTweets;
